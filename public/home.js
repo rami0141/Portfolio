f6ab708f87ab7c0df7395773fbafe2378d15ba1f
Cristina Zhang <rami0141@umn.edu>
2018-05-22 17:16:27 -0500

    form validation

--- public/home.js
+++ public/home.js
@@ -1,22 +1,35 @@
 $(document).ready(function () {
+$(".alert").hide();
+    var name = $("#name");
+    var email = $("#email");
+    var number = $("#number");
+    var message = $("#message");
 
     $('#submit').click(function () {
   		event.preventDefault();
+
+        if (!name.val().trim() || !email.val().trim() || !number.val().trim() || !message.val().trim())  {
+            //alert("please fill out form")
+            $('#error').modal('show'); 
+            return;          
+        }
+
   		var newMessage = {
 	    	name: $("#name").val(),
 	    	email: $("#email").val(),
 	    	number: $("#number").val(),
 	    	message: $("#message").val()
 		}
-    	$.post("/send", newMessage).done(function (data) {
-    		console.log(sent);
+
+    	$.post("/send", newMessage).done(function (data) {   		
     	});
 
-    $("#name").val("");
-    $("#email").val("");
-    $("#number").val("");
-    $("#message").val("");
-    
+        $('#myModal').modal('show'); 
+        $("#name").val("");
+        $("#email").val("");
+        $("#number").val("");
+        $("#message").val("");   
 	});
+
 
 });
